75d4afb5f49805e31aa8a71c2a84dbdcc9275bd2
ktkt9898 <kyletruschel@u.boisestate.edu>
2024-10-03 15:01:57 -0600

    first commit

---
+++ ArrayList.java
@@ -0,0 +1,156 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class ArrayList<T> implements IndexedUnsortedList<T> {
+    public static final int DEFAULT_CAPACITY = 10;
+    private T[] array;
+    private int rear;
+    private T element;
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList(int initialCapacity) {
+        array = (T[])(new Object[initialCapacity]);
+        rear = 0;
+    }
+
+    @Override
+    public void addToFront(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToFront'");
+    }
+
+    @Override
+    public void addToRear(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToRear'");
+    }
+
+    @Override
+    public void add(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void addAfter(T element, T target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addAfter'");
+    }
+
+    @Override
+    public void add(int index, T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+    }
+
+    @Override
+    public T remove(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public T remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public void set(int index, T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public int indexOf(T element) {
+        this.element = element;
+        int returnIndex = -1;
+        int currentIndex = 0;
+    
+        while (returnIndex == -1 && currentIndex < rear) {
+            // the '==' operater checks object address
+            if (element.equals(array[currentIndex])) {
+                returnIndex = currentIndex;
+            }
+                
+            else {
+                currentIndex++;
+            }
+        }
+        return returnIndex;
+    }
+
+    @Override
+    public T first() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return array[0];
+    }
+
+    @Override
+    public T last() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return array[rear-1];
+    }
+
+    @Override
+    public boolean contains(T target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return rear == 0;
+    }
+
+    @Override
+    public int size() {
+        return rear;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listIterator'");
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int startingIndex) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listIterator'");
+    }
+    
+}
