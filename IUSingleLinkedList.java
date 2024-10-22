091d3ced7bd47a08caa3f2467bfc4011e5d76062
ktkt9898 <kyletruschel@u.boisestate.edu>
2024-10-22 13:12:44 -0600

    LinkedList started

---
+++ IUSingleLinkedList.java
@@ -0,0 +1,209 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Single-Linked node-based structure implemenetation of the index unsorted list
+ * Includes a basic Iterator to support remove operations.
+ * @author Kyle Truschel and CS221-3 F24
+ */
+public class IUSingleLinkedList<T> implements IndexedUnsortedList<T>{
+    // Represents the beginning of the list.
+    private Node<T> head;
+    // Represents the end of the list.
+    private Node<T> tail;
+    // Similar to the rear in order to keep track of size.
+    private int size;
+    // Assists the Iterator in comparing changes.
+    private int versionNumber;
+
+    /**
+     * Initialize a new EMPTY list, no nodes to start.
+     */
+    public IUSingleLinkedList() {
+        // Trick to assign right to left of the same type.
+        this.head = this.tail = null;
+        this.size = 0;
+        this.versionNumber = 0;
+    }
+
+    @Override
+    public void addToFront(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToFront'");
+    }
+
+    @Override
+    public void addToRear(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToRear'");
+    }
+
+    @Override
+    public void add(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public void addAfter(T element, T target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addAfter'");
+    }
+
+    @Override
+    public void add(int index, T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public T removeFirst() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+    }
+
+    @Override
+    public T removeLast() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+    }
+
+    @Override
+    public T remove(T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public T remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public void set(int index, T element) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public T get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public int indexOf(T element) {
+        // Concept is to navigate through the list using node references, always start at the head.
+        // Check if the current node is equal to the node storing the desired element.
+        // If not, advance the current node and incremenet a current node count.
+        // If current is equal to null, one past the tail, then the node containing the element
+        // was not found.
+
+        Node<T> currentNode = head;
+        int currentIndex = 0;
+
+        // While loop since we do not know the exact size.
+        // The equals() method compares object contents.
+        // As long as the current node is not null, the end of the list, and the element is not found.
+        while (currentNode != null && !currentNode.getElement().equals(element)) {
+            // The getNextNode() method retrieves the address of the second node first, which then.
+            // overwrites the address of the actual current node.
+            // Right side of the statement is figured out first, and then the variable is updated.
+            currentNode = currentNode.getNextNode();
+
+            // The mode has shifted, now update the current index.
+            currentIndex++;
+        }
+
+        // If the currentNode is null, we have reached the end of the list.
+        if (currentNode == null) {
+            // Best practice to prevent a "short circuit" in a loop, assign currentIndex to -1 to
+            // only have ONE return statement.
+            currentIndex = -1;
+        }
+        // If the element was found, return the current index.
+        // Following the best practice, only ONE return statement should exist.
+        return currentIndex;
+    }
+
+    @Override
+    public T first() {
+        // Conditional check to see if an exception must be thrown according to the interface.
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        // Must retrieve the node's contents, in this case, the head's contents.
+        return head.getElement();
+    }
+
+    @Override
+    public T last() {
+        // Conditional check to see if an exception must be thrown according to the interface.
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        // Must retrieve the node's contents, in this case, the tail's contents.
+        return tail.getElement();
+    }
+
+    @Override
+    public boolean contains(T target) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // Emphasize clarity and concise information.
+        return size == 0; 
+        // If head is null, it cannot possibly wrong since if head is lost, the list is lost.
+        // return head == null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        for (T element : this) {
+            stringBuilder.append(element.toString());
+            stringBuilder.append(", ");
+        }
+
+        if (size() > 0) {
+            // Remove trailing comma.
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listIterator'");
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int startingIndex) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listIterator'");
+    }
+
+}
---
+++ Node.java
@@ -0,0 +1,61 @@
+/**
+ * A node class for creating a linked data strucutre.
+ * @author Kyle Truschel and CS221-3 F24
+ */
+
+public class Node<T> {
+    private T element;
+    private Node<T> nextNode;
+
+    /**
+     * Initialize a new node with a desired element.
+     * @param element a desired element to be stored.
+     */
+    public Node(T element) {
+        // Nodes are always a placeholder for an element.
+        this.element = element;
+        nextNode = null;
+    }
+
+    /**
+     * Initialize a new node with a given element and known next node.
+     * @param element a desired element to be stored.
+     * @param nexNode a known node in the chain/linked data structure.
+     */
+    public Node(T element, Node<T> nexNode) {
+        this.element = element;
+        this.nextNode = nextNode;
+    }
+
+    /**
+     * Display the desired element.
+     * @return an element value.
+     */
+    public T getElement() {
+        return element;
+    }
+
+    /**
+     * Overwrite an existing element.
+     * @param element a desired element to be stored.
+     */
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    /**
+     * Display the next node in the chain.
+     * @return the next node value.
+     */
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    /**
+     * Overwrite an existing known node.
+     * @param nextNode a desired node to be overwritten.
+     */
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+}
